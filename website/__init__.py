7afb9e920a549c411c5c70b5419651990e9e41e4
Kshirod Patel <kshirod.patel@gmail.com>
2025-12-18 18:32:19 +0530

    Deleted files

--- website/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = "@#4%#$FgVBX&>?&*#$"
-
-    from .views import views
-    from .auth import auth
