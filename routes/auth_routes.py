88627288e5e2d5f34a786f4f4119baf784caaa49
Kshirod Patel <kshirod.patel@gmail.com>
2025-12-23 20:10:29 +0530

    Fixed issues with global user

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,31 +1,44 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import User
+from flask_login import login_user, logout_user, login_required, current_user
 from extensions import db
+
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+# Route for login user
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter_by(email=email).first()
+        
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user, remember=True)
+                return redirect(url_for("view.home"))
+            else:
+                print("Invalid password")
+        else:
+            print("Invalid User")
+    return render_template("login.html", user=current_user)
 
+# Route for signup user
 @auth.route("/signup", methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        firstname = request.form.get('firstname').strip()
-        lastname = request.form.get('lastname').strip()
-        phone = request.form.get('phone').strip()
         email = request.form.get('email').strip()
+        username = request.form.get('username').strip()
         password = request.form.get('password').strip()
         confirm_password = request.form.get('confirm_password').strip()
 
         user = User.query.filter_by(email=email).first()
         if user:
-            print('User already exists')
+            print('User already exists.')
         else :
             if len(email) < 4:
                 flash("Email is too short", category="error")
-            elif len(firstname) < 3 or len(lastname) < 3 :
-                flash("Firstname and Lastname should be more than 3 characters", category="error")
             elif password != confirm_password:
                 flash("Password and Confirm Password should be same", category="error")
             elif len(password) < 7:
@@ -33,16 +46,25 @@ def signup():
             else:
                 # Create User
                 print("Create User")
-                new_user = User(email=email, firstname=firstname, lastname=lastname, phone=phone, password=generate_password_hash(password, method="scrypt", salt_length=16))
+                new_user = User(email=email, username=username, password=generate_password_hash(password, method="scrypt", salt_length=16))
                 db.session.add(new_user)
                 db.session.commit()
-                return redirect(url_for("view.home"))
-    return render_template("signup.html")
+                return redirect(url_for("auth.login"))
+    return render_template("signup.html", user=current_user)
 
+# Route for Logout
 @auth.route("/logout")
 def logout():
-    return render_template("logout.html")
+    return render_template("home.html")
 
+# Route for user profile
 @auth.route("/profile")
 def profile():
-    return render_template("profile.html")
+    return render_template("profile.html", user=current_user)
+
+# Route for user logout
+@auth.route("/sign-out")
+@login_required
+def sign_out():
+    logout_user()
+    return redirect(url_for("auth.login"))
--- routes/view_routes.py
+++ routes/view_routes.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, redirect, url_for
-
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import Post
+from extensions import db
 view = Blueprint("view", __name__)
 
 @view.route("/")
