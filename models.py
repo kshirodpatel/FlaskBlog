4783aec69dab5eacd5c1235d2f69c964216df501
Kshirod Patel <kshirod.patel@gmail.com>
2025-12-23 20:09:46 +0530

    Added comment and like table

--- models.py
+++ models.py
@@ -4,16 +4,34 @@ from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(120))
-    lastname = db.Column(db.String(120))
-    phone = db.Column(db.String(120), unique=True)
     email = db.Column(db.String(120), unique=True)
+    username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    # date_created = db.Column(db.dateTime(timezone=True), default=func.now())
+    posts = db.relationship('Post', backref='user', passive_deletes=True)
+    comments = db.relationship('Comment', backref='user', passive_deletes=True)
+    likes = db.relationship('Like', backref='user', passive_deletes=True)
 
-# class Post(db.Model):
-#     id: db.Column(db.Integer, primary_key=True)
-#     post_title: db.Column(db.String(150))
-#     post_content: db.Column(db.Text, nullable=False)
-#     # date_created: db.Column(db.DateTime(timezone=True), default=func.now())
-#     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False) 
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post_title = db.Column(db.Text, nullable=False)
+    post_content = db.Column(db.Text, nullable=False)
+    post_category = db.Column(db.Text, nullable=False)
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    comments = db.relationship('Comment', backref='post', passive_deletes=True)
+    likes = db.relationship('Like', backref='post', passive_deletes=True)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(
+        'post.id', ondelete="CASCADE"), nullable=False)
+    
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(
+        'post.id', ondelete="CASCADE"), nullable=False) 
