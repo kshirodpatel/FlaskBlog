a1d397b5873d30251f394d73ab92911b45b7c3e1
Kshirod Patel <kshirod.patel@gmail.com>
2025-12-24 19:34:22 +0530

    Added route for author

--- routes/view_routes.py
+++ routes/view_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from extensions import db
-from models import Post
+from models import Post, User
 view = Blueprint("view", __name__)
 
 @view.route("/")
@@ -9,3 +9,8 @@ view = Blueprint("view", __name__)
 def home():
     posts = Post.query.all()
     return render_template("home.html", posts=posts)
+
+@view.route("/authors")
+def authors():
+    users = User.query.all()
+    return render_template("/author/authors.html", users=users)
