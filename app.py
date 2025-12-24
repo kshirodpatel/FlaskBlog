b92d6a57e39f8c9ed71cb406f6651173c592b0d4
Kshirod Patel <kshirod.patel@gmail.com>
2025-12-24 16:53:19 +0530

    code cleanup

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 from os import path
 from models import User
 
-
 # Load env variables from .env into the system enviornment
 load_dotenv()
 
@@ -16,18 +15,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(instance_path, "database.db")}'
 db.init_app(app)
+
 # --- Blueprint Imports ---
 from routes.auth_routes import auth
 from routes.view_routes import view
+from routes.post_routes import post
 
 # --- Blueprint Registrations ---
 app.register_blueprint(auth)
 app.register_blueprint(view)
+app.register_blueprint(post)
 
 # Create DBs
 with app.app_context():
     db.create_all()
-
 
 @login_manager.user_loader
 def load_user(id):
