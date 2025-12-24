f5c2996eb625f6d95830bdc624823838e8cc9b3a
Kshirod Patel <kshirod.patel@gmail.com>
2025-12-24 16:54:29 +0530

    Code cleanup

--- routes/view_routes.py
+++ routes/view_routes.py
@@ -1,14 +1,11 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import Post
 from extensions import db
+from models import Post
 view = Blueprint("view", __name__)
 
 @view.route("/")
 @view.route("/home")
 def home():
-    return render_template("home.html")
-
-@view.route("/create-post")
-def create_post():
-    return render_template("create-post.html")
+    posts = Post.query.all()
+    return render_template("home.html", posts=posts)
